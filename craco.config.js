9b4b0f25e69b273eabbdf2d9892a2895a60317c3
dl11 <dl11developer@gmail.com>
2021-01-19 12:46:37 +0100

    config

---
+++ craco.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    webpack: {
+        configure: {
+            target: 'electron-renderer'
+        }
+    },
+    style: {
+        postcss: {
+          plugins: [
+            require('tailwindcss'),
+            require('autoprefixer'),
+          ],
+        },
+      },
+};
